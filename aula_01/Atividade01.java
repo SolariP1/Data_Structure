22e8410a50b6bd63506b3e2024fbe0f4799bcfd0
Ezequiel Sousa <ezequielllscf@gmail.com>
2026-07-30 20:30:42 -0300

    Refeactor: refeactoring code and updating README.md from 'aula01'

--- aula_01/Atividade01.java
+++ aula_01/Atividade01.java
@@ -1,42 +1,31 @@
 import java.util.Random;
 
-public class Atividade01 {
-    
-    public static void aux(int[] vetor) {
-        long startTime = System.nanoTime();
+public class Atividade01{
+    public static void main(String[] args) {
+        int vetor[] = new int[1000000];
+        Random ran = new Random();
+
         
-        int n = vetor.length;
-        
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (vetor[j] > vetor[j + 1]) {
-                    int tempo = vetor[j];
-                    vetor[j] = vetor[j + 1];
-                    vetor[j + 1] = tempo;
+        for(int i = 0; i<vetor.length; i++){
+            vetor[i] = ran.nextInt(10);
+        }
+
+
+        long startTime = System.nanoTime(); 
+
+        for(int i = 0; i<vetor.length; i++){
+            for(int j=0; j<vetor.length;j++){
+                if(vetor[i]< vetor[j]){
+                    int aux = vetor[i];
+                    vetor[i] = vetor[j];
+                    vetor [j]= aux;
                 }
             }
         }
-        
         long endTime = System.nanoTime();
-        long duracao = (endTime - startTime);
-        
-        System.out.println("Tempo: " + (duracao / 1000000.0) + " ms");
-    }
-    
-    public static void preencherVetor(int[] vetor) {
-        Random random = new Random();
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = random.nextInt(10000);
-        }
-    }
-    public static void main(String[] args) {
-        int[] tamanhos = {10, 100, 10000, 100000, 1000000};
-        
-        for (int tamanho : tamanhos) {
-            System.out.println( tamanho + " elementos:");
-            int[] vetor = new int[tamanho];
-            preencherVetor(vetor);
-            aux(vetor);
-        }
+        long duration = (endTime - startTime); // Tempo em nanossegundos
+
+        System.out.println("Tempo: " + (duration / 1000000.0) + " ms");
     }
 }
+
---
+++ aula_01/docs/README.md
@@ -0,0 +1,7 @@
+## Diretório de Documentação Markdown do Projeto
+
+___Ata_de_apresentacao.md___
+
+documentado no dia __30/07/2026__
+
+assunto: __Documentação do código e soluções__
